3f09ef0f8664c536a7c4d7307042e37127550c8a
Jamie Oglethorpe <JamieO53@jamieo.com>
2019-05-07 10:40:19 +0200

    Add simple tests for legacy code - Executable and Executables classes

---
+++ PowerShellAnalyzer/AnalyzeExecutables/Tests/Executable.tests.ps1
@@ -0,0 +1,40 @@
+﻿if (Get-Module AnalyzeExecutables -All) {
+	Remove-Module AnalyzeExecutables
+}
+Import-Module "$PSScriptRoot\..\bin\Debug\AnalyzeExecutables\AnalyzeExecutables.psm1"
+
+Describe "Executable" {
+	Context "Exists" {
+		[System.Management.Automation.Language.Token[]]$tokens=$null
+		[System.Management.Automation.Language.ParseError[]]$errors=$null
+		[System.Management.Automation.Language.ScriptBlockAst]$script=$null
+		$script = [System.Management.Automation.Language.Parser]::ParseInput('function dummy {}',  [ref]$tokens, [ref]$errors)
+		It "Creatable" {
+			New-Executable 'script' $script | should not Be $null
+		}
+	}
+	Context "Executable props" {
+		[System.Management.Automation.Language.Token[]]$tokens=$null
+		[System.Management.Automation.Language.ParseError[]]$errors=$null
+		[System.Management.Automation.Language.ScriptBlockAst]$script=$null
+		$script = [System.Management.Automation.Language.Parser]::ParseInput('function dummy {}',  [ref]$tokens, [ref]$errors)
+		[System.Management.Automation.Language.FunctionDefinitionAst]$function = $null
+		$function = $script.EndBlock.Statements[0]
+		$fnEx = New-Executable 'dummy' $function
+		It "Function name" { 
+			$fnEx.Name | should Be 'dummy' 
+		}
+		It "Function AST" {
+			$fnEx.Ast| should Be $function 
+		}
+		It "Function type name" {
+			$fnEx.TypeName | should Be 'FunctionDefinitionAst'
+		}
+		It "Function references" {
+			$fnEx.References.Count | should Be 0
+		}
+		It "Function referenced by" {
+			$fnEx.ReferencedBy.Count | should Be 0
+		}
+	}
+}
---
+++ PowerShellAnalyzer/AnalyzeExecutables/Tests/Executables.tests.ps1
@@ -0,0 +1,25 @@
+﻿if (Get-Module AnalyzeExecutables -All) {
+	Remove-Module AnalyzeExecutables
+}
+Import-Module "$PSScriptRoot\..\bin\Debug\AnalyzeExecutables\AnalyzeExecutables.psm1"
+
+Describe "Executables" {
+	Context "Exists" {
+		$scriptPath = "$testDrive\script.ps1"
+		'function dummy {}' | sc -Path $scriptPath -Encoding UTF8
+		It "Creatable" {
+			New-Executables $scriptPath | should not Be $null
+		}
+	}
+	Context "Executables content" {
+				$scriptPath = "$testDrive\script.ps1"
+		'function dummy {}' | sc -Path $scriptPath -Encoding UTF8
+		$ex = New-Executables $scriptPath
+		It "Script is executable" {
+			$ex.ex.ContainsKey('script.ps1') | should Be $true
+		}
+		It "Dummy function is executable" {
+			$ex.ex.ContainsKey('dummy') | should Be $true
+		}
+	}
+}
